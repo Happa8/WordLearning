f7aac56f67522d39e4f10d1e825c82c98ef5d98f
Happa8 <happa.eight@gmail.com>
2016-10-30 23:36:55 +0900

    [add] record mode

--- main.rb
+++ main.rb
@@ -3,34 +3,50 @@ require 'bundler'
 Bundler.require
 include SQLite3
 
-# DB関連
-$db = Database.new("./word.db")
+def record(keyword)
+  $db = Database.new("./word.db")
                                                                       
-if !$db.execute("SELECT tbl_name FROM sqlite_master WHERE TYPE == 'table'").flatten.include?("words")
-sql_create_table = <<SQL
-CREATE TABLE words (
-  en_word text NOT NULL ,
-  jp_word text );
+  if !$db.execute("SELECT tbl_name FROM sqlite_master WHERE TYPE == 'table'").flatten.include?("words")
+  sql_create_table = <<SQL
+  CREATE TABLE words (
+    en_word text NOT NULL ,
+    jp_word text );
 SQL
-$db.execute(sql_create_table)
+  $db.execute(sql_create_table)
+  end
+
+  target_url = "http://ejje.weblio.jp/content/" + keyword
+
+  agent = Mechanize.new
+  search_page = agent.get(target_url)
+  search_page.search('.content-explanation').each do |item|
+    jp_word = item.inner_text
+    if !$db.execute("SELECT en_word FROM words").flatten.include?("#{keyword}")
+      $db.execute("INSERT INTO words VALUES('#{keyword}', '#{jp_word}')")
+    end
+    jp_word_array = jp_word.split("、")
+    jp_word_array.each do |meaning|
+      puts "#{meaning}\n"
+    end
+  end
+  $db.close
 end
 
-keyword = ARGV[0]
-
-target_url = "http://ejje.weblio.jp/content/" + keyword
-
-agent = Mechanize.new
-search_page = agent.get(target_url)
-search_page.search('.content-explanation').each do |item|
-  jp_word = item.inner_text
-  if !$db.execute("SELECT en_word FROM words").flatten.include?("#{keyword}")
-  $db.execute("INSERT INTO words VALUES('#{keyword}', '#{jp_word}')")
-  end
-  jp_word_array = jp_word.split("、")
-  jp_word_array.each do |meaning|
-    puts "#{meaning}\n"
+def main
+  puts "\e[H\e[2J"
+  print "WordLearning\n"
+  print "\t\tcreated by Happa8\n"
+  print "serect mode\n\t1: record mode\n\t2: test mode\n>"
+  mode = STDIN.gets.chomp
+  case mode
+  when "1"
+   print "Please input the word\n>"
+   word = STDIN.gets.chomp
+   record(word)
+  else
+    print "Please select correct number!"
+    main
   end
 end
 
-p $db.execute2("SELECT * FROM words")
-$db.close
+main
