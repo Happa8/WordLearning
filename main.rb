da4b7a820b9e75b0f6ca4e5b3a60612100945dcc
Happa8 <happa.eight@gmail.com>
2016-10-30 10:39:37 +0900

    first commit

---
+++ main.rb
@@ -0,0 +1,14 @@
+# scraping.rb 
+require 'bundler'
+Bundler.require
+
+keyword = ARGV[0]
+
+target_url = "http://ejje.weblio.jp/content/" + keyword
+
+agent = Mechanize.new
+search_page = agent.get(target_url)
+search_page.search('.content-explanation').each do |item|
+  res = item.inner_text
+  p res.split("、")
+end
